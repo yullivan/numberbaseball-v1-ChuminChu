a7bdab5880bcabb2ee43aefcbd105b1ed9f4ac21
ChuminChu <139088490+ChuminChu@users.noreply.github.com>
2024-12-10 15:26:34 +0900

    숫자야구게임 v1

--- src/test/java/numberbaseball/ApplicationTests.java
+++ src/test/java/numberbaseball/ApplicationTests.java
@@ -3,11 +3,17 @@ package numberbaseball;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class ApplicationTests {
 
+
 	@Test
 	void contextLoads() {
+
 	}
 
 }
